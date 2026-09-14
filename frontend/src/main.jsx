78001421c1d1074cf2716ffdab71875b6dfc9244
Saheblal Shaikh <saheblalshaikh029@gmail.com>
2026-09-14 20:08:24 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -81,7 +81,7 @@ function BrandMark({ compact = false }) {
         <Link2 size={compact ? 18 : 24} />
       </span>
       <div>
-        <strong>LinkVaults</strong>
+        <strong>LinkVault</strong>
         {!compact && <span>Secure URL Manager</span>}
       </div>
     </div>
